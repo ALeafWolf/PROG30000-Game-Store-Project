ef1bb9288c56ffc1c8adede8597d1e7ea5c6abfa
ALeafWolf <aleafwolf@outlook.com>
2020-11-15 21:40:32 -0500

    project init

---
+++ Team WASD - Game Store Stock Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Team_WASD___Game_Store_Stock_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
